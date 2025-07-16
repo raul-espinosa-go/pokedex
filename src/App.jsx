13c02d2ad0017edea2769ccfe3d83d8c62afe63a
respinosa <raul.espinosa.go@gmail.com>
2025-07-16 14:38:09 +0200

    Refactor App component layout for improved structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import PokedexList from "@/components/PokedexList";
 import Header from "./components/Header";
+import Footer from "./components/Footer.jsx";
 import PokemonDetails from "./components/PokemonDetails";
 import "./App.css"; 
+import image from "./assets/image.png";
 
 function App() {
   return (
     <div className="flex flex-col h-screen overflow-hidden background">
-      <Header className="" />
+      <Header className="fixed top-0" />
       <div className="flex flex-row flex-1">
-        <PokemonDetails />
-        <PokedexList className="w-2/3 pt-4 overflow-hidden" />
+        {/* <img src={image} alt="" className="opacity-80 fixed top-0 z-20" /> */}
+        <PokemonDetails className="h-screen w-screen" />
+        <PokedexList className="w-4/5 h-screen" />
       </div>
+      <Footer className="w-full fixed bottom-0 shadow-pokeball shadow-white/20" />
     </div>
   );
 }
