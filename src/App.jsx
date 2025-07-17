8e73caeb0fe63475ff3949af065b842ba885c13e
respinosa <raul.espinosa.go@gmail.com>
2025-07-17 20:35:17 +0200

    Refactor App component to use Pokedex page; remove Main page and update layout with new scrollbar functionality in PokedexList

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 
 // Pages
-import Main from "@/pages/Main.jsx";
+import Pokedex from "@/pages/Pokedex.jsx";
+
+// Components
+import Footer from "@/components/Footer.jsx";
+import Header from "@/components/Header.jsx";
+
 import styles from "@/App.module.css";
 
 function App() {
   const location = useLocation();
 
   return (
-    <main className={`${styles["main-background"]}`}>
+    <main
+      className={`${styles["main-background"]} flex flex-col h-dvh overflow-hidden`}
+    >
+      
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Pokedex />} />
         {/* Add more routes as needed */}
       </Routes>
+      <Footer className="w-full fixed bottom-0" />
     </main>
   );
 }
---
+++ src/components/icons/AlignLeft.jsx
@@ -0,0 +1,22 @@
+export function AlignLeft(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M15 12H3m14 6H3M21 6H3"
+      />
+    </svg>
+  );
+}
+
+export default AlignLeft;
