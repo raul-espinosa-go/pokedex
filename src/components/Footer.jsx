4b9823ea5a034e88b8fab6876f45e3f128d21425
respinosa <raul.espinosa.go@gmail.com>
2025-07-17 21:08:34 +0200

    Update Footer component: change icon images to white version and adjust layout styles for consistency

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
-import Github from "../assets/github-mark.svg";
-import Linkedin from "../assets/InBug-Black.png";
+import Github from "../assets/github-mark-white.svg";
+import Linkedin from "../assets/InBug-White.png";
 
-import styles from "./Footer.module.css";
+import styles from "./Layout.module.css";
 
 function Footer({ className }) {
   return (
     <footer
-      className={`items-center z-10 ${className} ${styles["footer-background"]} border-t-4 border-pokemon-yellow`}
+      className={`items-center z-10 ${className} ${styles.background} border-t-4 border-pokemon-yellow`}
     >
-      <div className="py-3 sm:py-1 flex flex-row justify-end items-center gap-12 pr-8">
+      <div className="py-3 sm:py-1 flex flex-row justify-end items-center gap-8 px-8">
         <a
           href="https://github.com/raul-espinosa-go/pokedex"
           target="_blank"
-          className="flex flex-row items-center gap-2 text-dark font-normal text-xl cursor-pointer"
+          className="chip cursor-pointer"
         >
           <div className="">
             <img
               src={Github}
               alt="Github Icon"
-              className="w-6 sm:w-4 md:w-4 lg:w-6"
+              className="w-4 sm:w-4 md:w-4 lg:w-6"
             />
           </div>
           <p className="text-base md:text-base">Github</p>
@@ -26,13 +26,13 @@ function Footer({ className }) {
         <a
           href="https://www.linkedin.com/in/raulespinosagomez/"
           target="_blank"
-          className="flex flex-row justify-center items-center gap-2 text-dark font-normal text-xl cursor-pointer"
+          className="chip cursor-pointer"
         >
           <div className="">
             <img
               src={Linkedin}
               alt="Github Icon"
-              className="w-6 sm:w-4 md:w- lg:w-6"
+              className="w-4 sm:w-4 md:w-4 lg:w-6"
             />
           </div>
           <p className="text-base md:text-base">Linkedin</p>
