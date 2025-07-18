c09b47354e8f97ae6b3eaa49363d7fe983090cf6
respinosa <raul.espinosa.go@gmail.com>
2025-07-18 11:22:45 +0200

    Add new ArrowDown icon components: ArrowDown01, ArrowDown10, ArrowDownAZ, and ArrowDownZA

---
+++ src/components/icons/ArrowDownAZ.jsx
@@ -0,0 +1,22 @@
+export function ArrowDownAZ(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m3 16l4 4l4-4m-4 4V4m13 4h-5m0 2V6.5a2.5 2.5 0 0 1 5 0V10m-5 4h5l-5 6h5"
+      />
+    </svg>
+  );
+}
+
+export default ArrowDownAZ;
---
+++ src/components/icons/ArrowDownZA.jsx
@@ -0,0 +1,22 @@
+export function ArrowDownZA(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m3 16l4 4l4-4M7 4v16m8-16h5l-5 6h5m-5 10v-3.5a2.5 2.5 0 0 1 5 0V20m0-2h-5"
+      />
+    </svg>
+  );
+}
+
+export default ArrowDownZA;
