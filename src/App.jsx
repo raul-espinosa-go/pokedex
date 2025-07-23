bada8b6d4715e9af83fbe6fef04c38a5e9587c19
respinosa <raul.espinosa.go@gmail.com>
2025-07-23 17:53:12 +0200

    Add loading state management and Loader component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // Pages
 import Index from "@/pages/Index.jsx";
@@ -7,36 +8,41 @@ import PokemonDetails from "@/pages/[id].jsx";
 
 // Components
 import Footer from "@/components/Footer.jsx";
+import Loader from "@/components/Loader.jsx";
+
+import usePokedexStore from "./store/usePokedexStore";
 
 import styles from "@/App.module.css";
 
 function App() {
   const location = useLocation();
+  const loading = usePokedexStore((state) => state.loading);
+  const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    if (loading) {
+      setShowLoader(true); 
+    }
+  }, [loading, showLoader]);
 
   return (
     <main className={`flex flex-col justify-between h-dvh overflow-hidden`}>
       <Routes location={location} key={location.pathname}>
-        <Route
-            path="/"
-            element={<Index />}
-            />
+        <Route path="/" element={<Index />} />
         <Route path="/pokedex/">
           <Route
             index
             element={<Pokedex className={`${styles["main-background"]}`} />}
           />
-          <Route
-            path=":id"
-            element={
-              <PokemonDetails />
-              // <PokemonDetails className={`${styles["details-background"]}`} />
-            }
-          />
+          <Route path=":id" element={<PokemonDetails />} />
         </Route>
-        {/* Add more routes as needed */}
       </Routes>
       {location.pathname !== "/" && (
         <Footer className="w-full fixed bottom-0" />
+      )}
+
+      {showLoader && (
+        <Loader onComplete={() => setShowLoader(false)} show={loading} />
       )}
     </main>
   );
