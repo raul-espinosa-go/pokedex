4004fba94317c9455e743d3c04f31d92d6725932
respinosa <raul.espinosa.go@gmail.com>
2025-07-24 21:18:32 +0200

    Add Fullscreen icon component for enhanced UI functionality

---
+++ src/components/icons/Fullscreen.jsx
@@ -0,0 +1,24 @@
+export function Fullscreen(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="M3 7V5a2 2 0 0 1 2-2h2m10 0h2a2 2 0 0 1 2 2v2m0 10v2a2 2 0 0 1-2 2h-2M7 21H5a2 2 0 0 1-2-2v-2" />
+        <rect width="10" height="8" x="7" y="8" rx="1" />
+      </g>
+    </svg>
+  );
+}
+
+export default Fullscreen;
